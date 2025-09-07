ebd8f2ff5319ad33bd7192b91de9d7aaf5e78434
Siddham Gaonkar <sidgaonkar@gmail.com>
2025-09-07 08:05:02 +0530

    added

--- main.c
+++ main.c
@@ -9,7 +9,23 @@ unsigned char : 0 to 255
 %x-- hex
 %o-- octal
 %c-- char
+%p-- address
+
+&--address of variables
 */
+
+/*
+------------Variables--------------------
+variable name is assigned to memory address to retrieve the data
+<datatype> <Variable name>
+
+char myscore; //cariable defination
+myscore=25    // variable initiialization
+
+------------Variable Scope--------------------
+when the variable is created inside a function it is called local variable
+when the variable is vreated outside a function it is called Global Variable
+ */
 #include <stdio.h>
 
 int main()
@@ -21,6 +37,13 @@ int main()
     data1=160;
     data2=40;
     data3 = data1+data2;
-    printf("Total Distance form A2C is: %d",data3);
+
+    unsigned long int addressofdata1 = (unsigned long int)&data1; //typecasting pointer to a number
+
+    printf("Total Distance form A2C is: %d \n",data3);
+    printf("-------Printing the Address of the variable----------");
+    printf("address of data1:%p data2:%p data3:%p",&data1,&data2,&data3);
+
+
     return 0;
 }
