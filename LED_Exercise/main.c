092d23e9842750eedd2fa3a4dce43282cbe238c4
Siddham Gaonkar <sidgaonkar@gmail.com>
2025-09-16 17:19:19 +0530

    led

--- LED_Exercise/main.c
+++ LED_Exercise/main.c
@@ -8,7 +8,17 @@ int main(void) {
     uint32_t *ptrPortDOutReg = (uint32_t *)0x40020C14;  // GPIO port output data register
 
     //1. Enable clock access to GPIOD
+    *ptrCLKctrlreg |= (1 << 3); // Set the 3rd bit to enable clock for GPIOD
+    //clear the 24th and 25th bits
+    *ptrPortDModeReg &= 0xFCFFFFFF;
+    //make 24th bit position as 1
+    *ptrPortDModeReg |= 0x01000000;
+    //set 12th bit position to 1
+    *ptrPortDOutReg |= 0x00001000;
     
+
+
+
 
     return 0;
 }
